97fffd33a18ad7cfca61b2b8635aec862f31edd7
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-04-29 00:43:49 -0500

    Added basic functions

--- code/code/LimpiezaDatos.R
+++ code/code/LimpiezaDatos.R
@@ -3,10 +3,12 @@ setwd('../../data')
 getwd()
 
 #Lectura de archivo
-
-
+originalFile <- read.csv('hotel_bookings_original.csv', header = TRUE, stringsAsFactors = FALSE)
+modifiedFile <- originalFile
 #Limpieza de archivos
-
-
+ 
+View(originalFile)
+summary(originalFile)
 
 #Guardado de archivos
+write.csv(modifiedFile, 'hotel_bookings_modified.csv')
