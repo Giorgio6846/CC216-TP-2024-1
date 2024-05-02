8d7f24d230230d13c6346df224dbcbd43c75280e
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-05-01 20:28:00 -0500

    Conversion de datos

--- code/code/LimpiezaDatos.R
+++ code/code/LimpiezaDatos.R
@@ -4,11 +4,35 @@ getwd()
 
 #Lectura de archivo
 originalFile <- read.csv('hotel_bookings_original.csv', header = TRUE, stringsAsFactors = FALSE)
-modifiedFile <- originalFile
 #Limpieza de archivos
  
 View(originalFile)
 summary(originalFile)
 
+  #Ver el tipo de dato por cada columna
+  str(originalFile)
+
+  #Obtener la cantidad de NA en cada columna
+  colSums(is.na(originalFile))
+ 
+  #Conversion de columna de integer to bool
+    
+    #Columna: is_canceled
+    #Al no encontrar ningun dato faltante en la columna is_canceled se puede continuar con el cambio  
+    originalFile$is_canceled <- as.logical(originalFile$is_canceled)
+  
+    #Columna: is_repeated_guest
+    #Al no encontrar ningun dato faltante en la columna is_repeated_guest se puede continuar con el cambio
+    originalFile$is_repeated_guest <- as.logical(originalFile$is_repeated_guest)
+    
+    #Despues de la conversion a logical
+    str(originalFile)
+    
+  #Reconocimiento de datos de caracteres a lista para deteccion de anomalias
+
+    list = as.list(df)
+    print(list)
+    
+  
 #Guardado de archivos
-write.csv(modifiedFile, 'hotel_bookings_modified.csv')
+write.csv(originalFile, 'hotel_bookings_modified.csv')
